dcade3ef5fd2fb1a95d79cddf3e9b61f5792a03b
Himanshu Sharma <himanshusharma@Himanshus-MacBook-Pro.local>
2019-02-25 10:30:57 -0500

    data is now being properly accepted for probability calculation

--- ED/source/eq.py
+++ ED/source/eq.py
@@ -1,28 +1,29 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from equtils import DataLoad
+import numpy as np
 
 
 class EQ:
 
     def __init__(self):
         self.r = 25   #radius of gaussian distribution
-        self.dimensions = (30,30,30)
+        self.dimensions = [30,30,30]
         
     def makeGrid(self,region, dimensions):
-        grid = np.array(dimensions)
-        depths, depthstep = np.linspace(region[0][0],region[0][1], dimension[0],endpoint=False,retstep=True)
+        grid = np.empty(dimensions)
+        depths, depthstep = np.linspace(region[0][0],region[0][1], dimensions[0],endpoint=False,retstep=True)
         self.depths = depths + depthstep/2
-        latitudes, latstep = np.linspace(region[1][0],region[1][1], dimension[1],endpoint=False,retstep=True)
+        latitudes, latstep = np.linspace(region[1][0],region[1][1], dimensions[1],endpoint=False,retstep=True)
         self.latitudes = latitudes + latstep/2
-        longitudes, longstep = np.linspace(region[2][0],region[2][1], dimension[2],endpoint=False,retstep=True)
+        longitudes, longstep = np.linspace(region[2][0],region[2][1], dimensions[2],endpoint=False,retstep=True)
         self.longitudes = longitudes + longstep/2
-        grid = np.array(dimensions)
-        for i in range(dimensions[0]): #depth
-            for j in range(dimensions[1]): #latitude
-                for k in range(dimensions[2]): #longitude
-                    grid[i][j][k] = (depths[i],latitudes[j],longitudes[k])
-        self.grid = grid
+##        for i in range(dimensions[0]): #depth
+##            for j in range(dimensions[1]): #latitude
+##                for k in range(dimensions[2]): #longitude
+##                    grid[i][j][k] = (depths[i],latitudes[j],longitudes[k])
+##        self.grid = grid
 
     def labelProb(self,hypocenter,dimensions):
         #region [[latitudemin,latitudemax],[longitudemin,longitudemax],[depthmin,depthmax]]
@@ -31,19 +32,20 @@ class EQ:
         for i in range(dimensions[0]): #depth
             for j in range(dimensions[1]): #latitude
                 for k in range(dimensions[2]): #longitude
-                    pArray[i][j][k] = pMag(hypocenter,self.grid[i][j][k])
+                    pArray[i][j][k] = self.pMag(hypocenter,(self.depths[i],self.latitudes[j],self.longitudes[k]))
         return pArray
         
     def pMag(self,hypocenter, point):
-        d = sqdistance(hypocenter, point)
+        d = self.sqdistance(hypocenter, point)
         return np.exp(-d/self.r)
         
 
-    def sqdistance(point1, point2):
+    def sqdistance(self,point1, point2):
         #point[0]:depth, point[1]=phi  point[2]=theta
         R = 6378.137
-        x1,y1,z1 = (R-point1[0])*cos(point1[1])*cos(point1[2]),(R-point1[0])*cos(point1[1])*sin(point1[2]),(R-point1[0]*sin(point1[1]))
-        x2,y2,z2 = (R-point2[0])*cos(point2[1])*cos(point2[2]),(R-point2[0])*cos(point2[1])*sin(point2[2]),(R-point2[0]*sin(point2[1]))
+        x1,y1,z1 = (R-point1[0])*np.cos(point1[1])*np.cos(point1[2]),(R-point1[0])*np.cos(point1[1])*np.sin(point1[2]),(R-point1[0]*np.sin(point1[1]))
+        x2,y2,z2 = (R-point2[0])*np.cos(point2[1])*np.cos(point2[2]),(R-point2[0])*np.cos(point2[1])*np.sin(point2[2]),(R-point2[0]*np.sin(point2[1]))
+        print(x1,x2,y1,y2,z1,z2)
         xd = x2-x1
         yd = y2-y1
         zd = z2-z1
@@ -64,11 +66,11 @@ class EQ:
 ##            return self.func(x)
 
 
-    def preprocess(x):
+    def preprocess(self,x):
         return x.view(3, 2772)
 
 
-    def getEqModel():
+    def getEqModel(self):
 
         model = nn.Sequential(
             Lambda(preprocess),
@@ -76,7 +78,7 @@ class EQ:
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d((1,4))
+            nn.MaxPool2d((1,4)),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
@@ -125,10 +127,10 @@ class EQ:
         return model
         
 
-    def entropy_loss(input,target):
+    def entropy_loss(self,input,target):
         return torch.nn.functional.binary_cross_entropy(input, target)
 
-    def loss_batch(model, loss_func, xb, yb, opt=None):
+    def loss_batch(self,model, loss_func, xb, yb, opt=None):
         loss = loss_func(model(xb), yb)
 
         if opt is not None:
@@ -139,7 +141,7 @@ class EQ:
         return loss.item(), len(xb)
 
 
-    def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+    def fit(self,epochs, model, loss_func, opt, train_dl, valid_dl):
         for epoch in range(epochs):
             model.train()
             for xb, yb in train_dl:
@@ -155,29 +157,49 @@ class EQ:
             print(epoch, val_loss)
 
 
-    def setitup(train_ds, valid_ds, bs):
+    def setitup(self):
+        self.makeGrid(self.region,self.dimensions)
+        labelpd = []
+        for i in range(len(self.yb)):
+            pd = self.labelProb(self.yb[i],self.dimensions)
+            labelpd.append(pd)
+        self.ypd = labelpd
+
+        #dt = DataLoad()
+        #dfx, dfy = dt.loadData()
+
+        #region = [(dt.latitudeMin,dt.latitudeMax),(dt.longitudeMin,dt.longitudeMax),(dt.depthMin,dt.depthMax)]
+        #self.makegrid(region,self.dimensions)
+
+        #yb = []
+        #for i in range(len(dfy)):
+         #   pd = self.labelProb(df.iloc[i].values,self.dimensions)
+          #  yb.append(pd)
+        #self.yb = yb
+        #self.xb = dfx.as_matrix()
+
+    def getReadyData(self):
         dt = DataLoad()
-        dfx, dfy = dt.loadData()
-
-        region = [(dt.latitudeMin,dt.latitudeMax),(dt.longitudeMin,dt.longitudeMax),(dt.depthMin,dt.depthMax)]
-        self.makegrid(region,self.dimensions)
-
-        yb = []
-        for i in range(len(dfy)):
-            pd = self.labelProf(df.iloc[i].values,self.dimensions)
-            yb.append(pd)
-        self.yb = yb
-        self.xb = dfx.as_matrix()
-
-    
-
+        self.xb, self.yb = dt.loadData()
+        self.region = dt.getDimensions()
+        
 
     if __name__ == "main":
+
+        epochs = 10
+        lr = 0.0001
+        
 
         model = getEqModel()
         opt = optim.SGD(model.parameters(),lr=lr)
 
-        train_dl, valid_dl = get_data(train_ds, valid_ds, bs)
+        train_ds = TensorDataset(self.xb,self.ypd)
+        valid_ds = TensorDataset(self.xb,self.ypd)
+
+        train_dl = DataLoader(train_ds, batch_size=8)
+        valid_dl = DataLoader(valid_ds, batch_size=16)
+        
+#        train_dl, valid_dl = get_data(train_ds, valid_ds, bs)
 
         loss_func = entropy_loss
 
--- ED/source/equtils.py
+++ ED/source/equtils.py
@@ -11,6 +11,7 @@ from torch.utils.data import DataLoader
 import torch
 from torch.utils.data import TensorDataset
 import itertools
+from obspy.core import UTCDateTime
 
 
 class DataLoad:
@@ -25,35 +26,47 @@ class DataLoad:
         path = "../eqdata/" + x[0] + "/" + x[0] + x[1] + "/" + x[6]
         print('hello')
         st = read(path)
-        headers_x = list(st[0].stats.keys())
+        headers_x = list(st[0].stats.keys())[0:10]
+        print(headers_x)
         headers_y = ['depth','latitude','longitude']
         dfy = pd.DataFrame(columns = headers_y)
-        #    items = list(st[0].stats.values())
-        dfx =  pd.DataFrame(columns=headers_x)
-            
+        dfx =  pd.DataFrame(columns = headers_x)
+        data = []
+        label = []
         for line in buf:
             try:
                 x = line[0].split(" ")
-                path = "../eqdata/" + x[0] + "/" + x[0] + x[1] + "/" + x[6]
-                print(path)
+                path = "../eqdata/" + x[0] + "/" + x[0] + x[1] + "/new/" + x[6]
+                dt = UTCDateTime(x[0]+'-'+x[1]+'-'+x[2]+'T'+x[3]+':'+x[4]+':'+x[5])
                 if os.path.isfile(path):
                     st = read(path)
+                    st = st.slice(dt-2,dt+18)
+                    dp = np.empty([len(st),len(st[0].data)])
+                    lp = [float(x[10]),float(x[7]),float(x[8])]
+                    #print(len(st))
                     for i in range(len(st)):
+                        dp[i] = st[i].data
                         items = list(st[i].stats.values())
+                        values = items[0:10]
+#                        values.append(items[9])
+                        #print(values)
                         dfy.loc[len(dfy)] = [x[10],x[7],x[8]]
-                        dfx.loc[len(dfx)] = items
-            except:
-                continue
+                        dfx.loc[len(dfx)] = values
+                    label.append(lp)
+                    data.append(dp)
+                    
+            except Exception as ex:
+                print(ex)
 
-        self.latitudeMax = dfy['latitude'].max()
-        self.latitudeMin = dfy['latitude'].min()
-        self.longitudeMax = dfy['longitude'].max()
-        self.longitudeMin = dfy['longitude'].min()
-        self.depthMax = dfy['depth'].max()
-        self.depthMin = dfy['depth'].min()
+        self.latitudeMax = float(dfy['latitude'].max())
+        self.latitudeMin = float(dfy['latitude'].min())
+        self.longitudeMax = float(dfy['longitude'].max())
+        self.longitudeMin = float(dfy['longitude'].min())
+        self.depthMax = float(dfy['depth'].max())
+        self.depthMin = float(dfy['depth'].min())
         self.dfx = dfx
         self.dfy = dfy
-        return dfx,dfy # return matrix of dfx and dfy ??
+        return np.asarray(data),label # return matrix of dfx and dfy ??
 
-    def getDimensions():
-        return [[self.latitudeMin,self.latitudeMax],[self.longitudeMin,self.longitudeMax],[self.depthMin,self.depthMax]]
+    def getDimensions(self):
+        return [[self.depthMin,self.depthMax],[self.latitudeMin,self.latitudeMax],[self.longitudeMin,self.longitudeMax]]
